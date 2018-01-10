0ad756df047387a6e58d5bf2110a67d6febb7ca4
ApolloEon <35278450+ApolloEon@users.noreply.github.com>
2018-01-10 13:21:36 -0800

    Answered questions for the assignment.

--- 01 hello.py
+++ 01 hello.py
@@ -5,3 +5,9 @@ import sys
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
 print('Hello, Class!')
+print("I am Apollo Eon.")
+print("It's hard to choose a favorite, but I love the games Tekken Tag Tournament 2 and League of Legends.")
+print("Concerns that I have for this class (Z399) include having trouble understanding materials then falling behind.")
+print("Something I'm excited about in general is being on the path to becoming a game designer.")
+print("My stackoverflow.com user number is 9195836.")
+print("The URL to my github.com profile is https://github.com/ApolloEon")
